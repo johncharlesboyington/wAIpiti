4dcee840a070e9f0c203e7c0939ecb05b524ee31
johncharlesboyington <johncharlesboyington@gmail.com>
2021-01-11 11:05:26 -0600

    corrected latitude and longitude for x and y points

--- elk/visualizer/mpl_visualizer.py
+++ elk/visualizer/mpl_visualizer.py
@@ -5,33 +5,32 @@ from gps_container import get_data
 
 def visualize():
     """Docstring."""
-    
+
     # import the data
     data = get_data()
-    
+
     # let's look at a smaller dataset
-    data = data[:500]
+    data = data[:1000]
 
     # calculate the domain
-    xrange = (min(data, key=lambda x: x.lat).lat,
-              max(data, key=lambda x: x.lat).lat)
-    yrange = (min(data, key=lambda x: x.long).long,
+    xrange = (min(data, key=lambda x: x.long).long,
               max(data, key=lambda x: x.long).long)
+    yrange = (min(data, key=lambda x: x.lat).lat,
+              max(data, key=lambda x: x.lat).lat)
 
     # setup the plotting environment
     fig = plt.figure(0)
     ax = fig.add_subplot(111)
     ax.set_xlim(*xrange)
     ax.set_ylim(*yrange)
-    point = ax.plot(data[0].lat, data[0].long, c='blue', marker='o', ms=2.5)[0]
+    point = ax.plot(data[0].long, data[0].lat, c='blue', marker='o', ms=2.5)[0]
     text = ax.text(xrange[0], yrange[0], data[0].timestamp)
-    
+
     def animate(i):
         text.set_text(data[i].timestamp)
         point.set_xdata(data[i].lat)
         point.set_ydata(data[i].long)
         return
-    
 
     ani = animation.FuncAnimation(fig, animate, frames=100, interval=250,
                                   repeat_delay=2000)
