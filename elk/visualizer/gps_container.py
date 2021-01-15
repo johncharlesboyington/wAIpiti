3cb853cf978d123f828560913a3521a9883b221c
johncharlesboyington <johncharlesboyington@gmail.com>
2021-01-15 12:44:09 -0600

    added date and time floats between [0, 1]

--- elk/visualizer/gps_container.py
+++ elk/visualizer/gps_container.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class GPS_Container():
     """Docstring."""
     
@@ -12,6 +15,29 @@ class GPS_Container():
         self.long = float(loc_long)
         self.lat = float(loc_lat)
         self.temp = float(external_temperature) if external_temperature else None
+        self.convert_timestamp_to_absolute()
+        return
+    
+    def convert_timestamp_to_absolute(self):
+        """Docstring."""
+        date_str, time_str = self.timestamp.split()
+        
+        # date
+        y, m, d = (int(v) for v in date_str.split('-'))
+        
+        # determine if leap year
+        if (((not y % 4) and (not y % 100) and (not y % 400))
+            or ((not y % 4) and (y % 100))):
+            days_per_month = (31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+        else:
+            days_per_month = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+        
+        self.date = ((np.sum(days_per_month[0:m - 1]) + d)
+                     / np.sum(days_per_month))
+        
+        # time
+        h, m, s = (float(v) for v in time_str.split(':'))
+        self.time = (h * 3600 + m * 60 + s) / (24 * 3600)
         return
 
 
@@ -26,5 +52,14 @@ def get_data():
     return data
 
 
+def store_one():
+    """Docstring"""
+    with open('../data/elk_in_southwestern_alberta.csv') as F:
+        lines = F.readlines()
+    for line in lines[1:]:
+        line = line.split(',')
+        return GPS_Container(*line)
+
+
 if __name__ == '__main__':
-    get_data()
+    store_one()
